26fa91f7ea93309fc05a0d5fa457dc1cd074f931
Michael Galloway <michael.galloway9@gmail.com>
2021-05-12 09:47:59 -0500

    Cleaned up About.js file

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,22 +16,22 @@ const mainPage = () => {
                             <div className="row">
                                 <div className="col-md-3">
                                     <img id="pic" src={img} width="100%" height="auto%" />
+                                    </div>
+
+                                    <div className="col-md-8">
+
+                                    <p>My name is Michael Galloway.  I was born and raised in Alaska.  I moved to the greater Houston area in 2015.  I enjoy hiking the outdoors, photography, traveling the world, sports, good food and craft beer.</p>
+
+                                    <p>I have 16+ years of experience working supply chain and materials in the oil and gas industry with a vast knowledge in Warehousing and Inventory Management.  Highly skilled in analytics with a focus on zero inventory mindset and cost savings.</p>
+
+                                    <p>In 2021, I enrolled in Rice University's Full Stack Developer Coding Boot Camp.  After 12 intense weeks, I've been exposed to (in no particular order): HTML, CSS, JavaScript, jQuery, Bootstrap, APIs, JSON, AJAX, React.js, PWAs, Heroku, Git, GitHub, MySQL, MongoDB, Templating Engines, Sessions, Writing tests, Node.js, Express.js, Creating APIs, MVC, User Authentication, ORM, Algorithms, and Data Structures.</p>
+
                                 </div>
-
-                                <div className="col-md-8">
-
-                                <p>My name is Michael Galloway.  I was born and raised in Alaska.  I moved to the greater Houston area in 2015.  I enjoy hiking the outdoors, photography, traveling the world, sports, good food and craft beer.</p>
-    
-                                <p>I have 16+ years of experience working supply chain and materials in the oil and gas industry with a vast knowledge in Warehousing and Inventory Management.  Highly skilled in analytics with a focus on zero inventory mindset and cost savings.</p>
-        
-                                <p>In 2021, I enrolled in Rice University's Full Stack Developer Coding Boot Camp.  After 12 intense weeks, I've been exposed to (in no particular order): HTML, CSS, JavaScript, jQuery, Bootstrap, APIs, JSON, AJAX, React.js, PWAs, Heroku, Git, GitHub, MySQL, MongoDB, Templating Engines, Sessions, Writing tests, Node.js, Express.js, Creating APIs, MVC, User Authentication, ORM, Algorithms, and Data Structures.</p>
-
-   </div>
-    </div>
-       </blockquote>
-    </div>
-</div>
-</div>
+                            </div>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
